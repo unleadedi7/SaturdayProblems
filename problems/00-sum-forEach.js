de0b938bc7a415b388daec92cb893924d7d1d065
ranpha07 <ranpha07@gmail.com>
2022-08-06 13:05:26 -0400

    Problem 00

--- problems/00-sum-forEach.js
+++ problems/00-sum-forEach.js
@@ -12,7 +12,14 @@ console.log(sumForEach([])); // 0
 */
 
 let sumForEach = function(arr) {
-  // Your code here
+  let counter = 0;
+
+  arr.forEach(function(arr){
+    counter+= arr;
+
+
+  });
+  return counter;
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
